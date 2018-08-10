e4fd937595a7078fb83d3b67671610c32d2d6319
Zihan Zhang <zzhlovesly@outlook.com>
2018-08-10 23:57:43 +0800

    user api, server side validation, authentication

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,10 @@ const port = process.env.PORT || 5000;
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
+// extended?
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // DB config
 const db = require('./config/keys').mongoURI;
 // Connect
@@ -16,7 +22,11 @@ mongoose
     .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send('Hello Asshose'));
+// passport 
+app.use(passport.initialize());
+
+// passport config
+require('./config/passport')(passport);
 
 // use routes
 app.use('/api/users', users);
