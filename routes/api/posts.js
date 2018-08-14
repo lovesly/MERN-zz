95b546173f68f356e772724ef3736c9ce42618cd
Zihan Zhang <zzhlovesly@outlook.com>
2018-08-15 00:56:29 +0800

    clean up

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -29,11 +29,6 @@ router.get('/:id', (req, res) => {
         .catch(err => res.status(404).json({ err }));
 });
 
-// edit/update?
-
-
-
-
 // @route POST api/posts
 // @desc Create post
 // @access Private
@@ -45,7 +40,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     const newPost = new Post({
         text: req.body.text,
         name: req.body.name,
-        avatar: req.body.name,   
+        avatar: req.body.avatar,   
         user: req.user.id,
     });
 
@@ -58,6 +53,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
     const errors = {};
     // we can use user model to do thist right? 
+    // why do we need Profile model????
     Profile.findOne({ user: req.user.id })
         .then(profile => {
             if (!profile) {
@@ -87,6 +83,7 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
 router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
     const errors = {};
     // we can use user model to do thist right? 
+    // why do we need Profile model????
     Profile.findOne({ user: req.user.id })
         .then(profile => {
             if (!profile) {
@@ -145,5 +142,74 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
         });
 });
 
+// @route POST  api/posts/comment/:id
+// @desc Add comment to post
+// @access Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    // we can use user model to do thist right? 
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                // the question is, Is a user without profile considered as a valid user?
+                errors.noprofile = 'User profile not found';
+                return res.status(404).json(errors);
+            } else {
+                // if already liked, return
+                Post.findById(req.params.id)
+                    .then(post => {
+                        const newComment = {
+                            text: req.body.text,
+                            name: req.body.name,
+                            avatar: req.body.avatar,
+                            user: req.user.id,
+                        };
+                        post.comments.push(newComment);
+                        return post.save().then(() => res.json({ post }));
+                    })
+                    .catch(err => res.status(404).json({ err }));
+            }
+        });
+});
+
+// @route DELETE  api/posts/:id/comment/:comment_id
+// @desc Delete a comment from post
+// @access Private
+// delete or post? we are using post in unlike routes
+router.delete('/:id/comment/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+    // we can use user model to do thist right? 
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {ß
+                errors.noprofile = 'User profile not found';
+                return res.status(404).json(errors);
+            } else {
+                Post.findById(req.params.id)
+                    .then(post => {
+                        // find the comment and check the author of comment
+                        const commentIndex = post.comments.findIndex(el => el._id.toString() === req.params.comment_id);
+                        if (commentIndex === -1) {
+                            errors.comment = 'Comment not found with given comment id'
+                            return res.status(404).json({ errors });
+                        }
+                        // compare the user id
+                        if (post.comments[commentIndex].user.toString() === req.user.id || post.user.toString() === req.user.id) {
+                            // toDo owner of the post should be able to delete comments.
+                            // valid comment. 
+                            post.comments.splice(commentIndex, 1);
+                            post.save().then(() => res.json({ post }));
+                        } else {
+                            errors.comment = 'User not authorized'
+                            return res.status(401).json({ errors });
+                        }
+                    })
+                    .catch(err => res.status(404).json({ err }));
+            }
+        });
+});
 
 module.exports = router;
