ce03b943f0e622f1f952b5a245b91dd8db2f76c3
Zihan Zhang <zzhlovesly@outlook.com>
2018-08-15 00:53:26 +0800

    start client work, add several components

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route path="/" component={ Landing } exact={ true } />
+          <div className="container">
+            <Route path="/register" component={ Register } exact={ true } />
+            <Route path="/login" component={ Login } exact={ true } />
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
